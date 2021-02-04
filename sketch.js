c15e7a9534aafc2d849bb5bd6a4ab8f5cfa69283
AbeerTheGreatest <74409055+BRODUD@users.noreply.github.com>
2021-02-04 13:32:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var canva;
+var contestantCount, database, quiz, question, contestant;
+var gameState = 0
+function setup(){
+database = firebase.database;
+  canvas = createCanvas(850,400);
+
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
